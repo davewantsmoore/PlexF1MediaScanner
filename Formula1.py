bb8ca6d3db73dd0079b9796a5dcc29bc7a6caf11
davewantsmoore <49543802+davewantsmoore@users.noreply.github.com>
2025-06-07 21:50:46 +1000

    Update egortech regex to also match HD releases
    Stop regex before quality (as quality is not being used).
    
    He is also naming some of his files without "Grand Prix", which is preventing regex from matching.... trying to work on a regex(es) for this case too.

--- Formula1.py
+++ Formula1.py
@@ -41,7 +41,7 @@ regexes = [
     # Formula.1.2020x05.70th-Anniversary-GB.Race.SkyF1HD.1080p/02.Race.Session.mp4
     ("smcgill1969", 'Formula.1[\._ ](?P<year>[0-9]{4})x(?P<raceno>[0-9]{2})[\._ ](?P<location>.*)[\._ ](?P<session>.*?).SkyF1U?HD.(1080p|SD)/(?P<episode>.*?)[\._ ](?P<description>.*?).mp4'),
     # 01.F1.2024.R24.Abu.Dhabi.Grand.Prix.Drivers.Press.Conference.Sky.Sports.F1.UHD.2160P.mkv
-    ("egortech", '(?P<episode>[0-9]{2}).F1.(?P<year>[0-9]{4}).R(?P<raceno>[0-9]{2}).(?P<location>.*?).Grand.Prix.(?P<description>.*?).Sky.Sports.F1.UHD.(?P<quality>[0-9]+(P|p)).mkv'),
+    ("egortech", '(?P<episode>[0-9]{2}).F1.(?P<year>[0-9]{4}).R(?P<raceno>[0-9]{2}).(?P<location>.*?).Grand.Prix.(?P<description>.*?).Sky*?'),
     # Fallback should always be last
     ("fallback", '.*.(mp4|mkv)')
 ]
